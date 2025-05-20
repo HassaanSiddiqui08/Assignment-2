ec4b5d8655490638f8e8966cb008667b47db4f88
HassaanSiddiqui08 <2692056@schoolsnet.act.edu.au>
2025-05-20 22:43:04 +1000

    created conditions lesson

--- Questacode1/Lessons/Conditions.Designer.cs
+++ Questacode1/Lessons/Conditions.Designer.cs
@@ -28,56 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBox1 = new TextBox();
-            pictureBox1 = new PictureBox();
             grpSubTopics = new GroupBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            richTextBox1 = new RichTextBox();
+            label1 = new Label();
+            panel1 = new Panel();
+            labelCode = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            btnDone = new Button();
             SuspendLayout();
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(100, 561);
-            textBox1.Multiline = true;
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(845, 125);
-            textBox1.TabIndex = 10;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Location = new Point(201, 82);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(676, 350);
-            pictureBox1.TabIndex = 9;
-            pictureBox1.TabStop = false;
             // 
             // grpSubTopics
             // 
-            grpSubTopics.Location = new Point(976, 12);
+            grpSubTopics.Location = new Point(1040, 12);
             grpSubTopics.Name = "grpSubTopics";
             grpSubTopics.Size = new Size(190, 541);
-            grpSubTopics.TabIndex = 8;
+            grpSubTopics.TabIndex = 7;
             grpSubTopics.TabStop = false;
             grpSubTopics.Text = "Sub Topics";
             // 
-            // Conditions
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(8, 12);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(1026, 406);
+            richTextBox1.TabIndex = 8;
+            richTextBox1.Text = "";
+            richTextBox1.LinkClicked += richTextBox1_LinkClicked;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(71, 444);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 9;
+            label1.Text = "label1";
+            label1.MouseDown += label1_MouseDown;
+            // 
+            // panel1
+            // 
+            panel1.AllowDrop = true;
+            panel1.BackColor = SystemColors.Control;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.ForeColor = Color.RoyalBlue;
+            panel1.Location = new Point(554, 498);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(209, 46);
+            panel1.TabIndex = 10;
+            panel1.DragDrop += panel1_DragDrop;
+            panel1.DragEnter += panel1_DragEnter;
+            // 
+            // labelCode
+            // 
+            labelCode.Location = new Point(430, 431);
+            labelCode.Name = "labelCode";
+            labelCode.Size = new Size(524, 244);
+            labelCode.TabIndex = 11;
+            labelCode.Text = "labelcode\r\n";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(71, 498);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 12;
+            label2.Text = "label2";
+            label2.MouseDown += label2_MouseDown;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(71, 554);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 13;
+            label3.Text = "label3";
+            label3.MouseDown += label3_MouseDown;
+            // 
+            // btnDone
+            // 
+            btnDone.Dock = DockStyle.Bottom;
+            btnDone.Location = new Point(0, 646);
+            btnDone.Name = "btnDone";
+            btnDone.Size = new Size(1242, 38);
+            btnDone.TabIndex = 14;
+            btnDone.Text = "Done";
+            btnDone.UseVisualStyleBackColor = true;
+            btnDone.Click += btnDone_Click;
+            // 
+            // Operators
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1285, 759);
-            Controls.Add(textBox1);
-            Controls.Add(pictureBox1);
+            ClientSize = new Size(1242, 684);
+            Controls.Add(btnDone);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            Controls.Add(label1);
+            Controls.Add(richTextBox1);
             Controls.Add(grpSubTopics);
-            Name = "Conditions";
-            Text = "Conditions";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            Controls.Add(labelCode);
+            Name = "Operators";
+            Text = "Operators";
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private TextBox textBox1;
-        private PictureBox pictureBox1;
         private GroupBox grpSubTopics;
+        private RichTextBox richTextBox1;
+        private Label label1;
+        private Panel panel1;
+        private Label labelCode;
+        private Label label2;
+        private Label label3;
+        private Button btnDone;
     }
 }
