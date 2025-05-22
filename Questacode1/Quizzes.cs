dd6666ee2fe468d61766edabeb0ea69ee49d111a
HassaanSiddiqui08 <2692056@schoolsnet.act.edu.au>
2025-05-22 21:01:32 +1000

    more questions added

--- Questacode1/Quizzes.cs
+++ Questacode1/Quizzes.cs
@@ -24,7 +24,7 @@ namespace Questacode1
             ShowQuestion();
 
             labelScore.Text = $"Score: {score}";
-            
+
         }
 
         private void LoadQuestions()
@@ -48,10 +48,114 @@ namespace Questacode1
                     QuestionText = "What is a procedure?",
                     Options = new string[] { "crazy person", "a good boy", "set of instructions" },
                     CorrectAnswerIndex = 2,
+                },
+                new Question
+                {
+                    QuestionText = "If you have a box to keep numbers, what is that box called in coding?",
+                    Options = new string[] { "A table", "A variable", "A chair" },
+                    CorrectAnswerIndex = 1,
+                },
+                new Question
+                {
+                    QuestionText = "Which word tells the computer that something is a whole number?",
+                    Options = new string[] { "text", "decimal", "integer" },
+                    CorrectAnswerIndex = 2,
+                },
+                new Question
+                {
+                    QuestionText = "What do we use to store words or sentences in coding?",
+                    Options = new string[] { "number", "string", "picture" },
+                    CorrectAnswerIndex = 1,
+                },
+
+                // Operators
+                new Question
+                {
+                    QuestionText = "What does the '+' sign do in coding?",
+                    Options = new string[] { "Makes things smaller", "Adds numbers together", "Deletes things" },
+                    CorrectAnswerIndex = 1,
+                },
+                new Question
+                {
+                    QuestionText = "If you want to multiply two numbers, which sign do you use?",
+                    Options = new string[] { "-", "*", "/" },
+                    CorrectAnswerIndex = 1,
+                },
+                new Question
+                {
+                    QuestionText = "What does the '=' sign mean in coding?",
+                    Options = new string[] { "It's equal to", "It gives a value to something", "It's a question mark" },
+                    CorrectAnswerIndex = 1,
+                },
+                new Question
+                {
+                    QuestionText = "Which sign means 'not equal to'?",
+                    Options = new string[] { "==", "!=", "<=" },
+                    CorrectAnswerIndex = 1,
+                },
+
+                // Conditions (If/Else)
+                new Question
+                {
+                    QuestionText = "If it's raining, what should you do?",
+                    Options = new string[] { "Go outside without umbrella", "Stay inside or use an umbrella", "Sing loudly" },
+                    CorrectAnswerIndex = 1,
+                },
+                new Question
+                {
+                    QuestionText = "In coding, what word helps the computer make a choice?",
+                    Options = new string[] { "Run", "If", "Stop" },
+                    CorrectAnswerIndex = 1,
+                },
+                new Question
+                {
+                    QuestionText = "What happens if an 'if' condition is not true?",
+                    Options = new string[] { "The computer stops", "It tries the 'else' part", "It asks for help" },
+                    CorrectAnswerIndex = 1,
+                },
+
+                // Loops
+                new Question
+                {
+                    QuestionText = "If you want the computer to do something many times, what do you use?",
+                    Options = new string[] { "A single command", "A loop", "A pause" },
+                    CorrectAnswerIndex = 1,
+                },
+                new Question
+                {
+                    QuestionText = "Which loop repeats something a certain number of times?",
+                    Options = new string[] { "While loop", "For loop", "If loop" },
+                    CorrectAnswerIndex = 1,
+                },
+                new Question
+                {
+                    QuestionText = "What does a 'while' loop do?",
+                    Options = new string[] { "Runs once", "Runs only if it's correct", "Keeps running as long as a condition is true" },
+                    CorrectAnswerIndex = 2,
+                },
+
+                // Functions/Methods
+                new Question
+                {
+                    QuestionText = "What is a small block of code that does a specific job?",
+                    Options = new string[] { "A book", "A function (or method)", "A game" },
+                    CorrectAnswerIndex = 1,
+                },
+                new Question
+                {
+                    QuestionText = "When you 'call' a function, what are you doing?",
+                    Options = new string[] { "Talking to it", "Making it do its job", "Ignoring it" },
+                    CorrectAnswerIndex = 1,
+                },
+                new Question
+                {
+                    QuestionText = "Why do we use functions in coding?",
+                    Options = new string[] { "To make code longer", "To organize code and reuse it", "To make code disappear" },
+                    CorrectAnswerIndex = 1,
                 }
-                //todo: add more questions
             };
-            
+
+
             questions = questions.OrderBy(x => Guid.NewGuid()).ToList();
         }
 
@@ -136,7 +240,7 @@ namespace Questacode1
 
         }
 
-       
+
         private void Quizzes_Load(object sender, EventArgs e)
         {
 
